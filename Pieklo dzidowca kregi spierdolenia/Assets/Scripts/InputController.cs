f8bbe4625460d267c361afe3b7bd3f605c5dd99c
Tails exe <mareksmrekrg@gmail.com>
2020-08-21 19:15:11 +0200

    Readded scripts

---
+++ Pieklo dzidowca kregi spierdolenia/Assets/Scripts/InputController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This is key mapping
+public enum Movement
+{
+    up = KeyCode.W,
+    down = KeyCode.S,
+    left = KeyCode.A,
+    right = KeyCode.D
+}
+
+
+
+public class InputController : Singleton<InputController>
+{
+    // This is status of input, holds bool values for each input
+    public struct MovementInputStatus
+    {
+        public bool up;
+        public bool down;
+        public bool left;
+        public bool right;
+    }
+    // this is instance of above struct, unless we can make the strut a "singleton" this will do for now.
+    public MovementInputStatus movementInputStatus;
+    public Vector3 mousePositionFlat;
+
+    protected InputController() { }
+
+
+
+    private void Update()
+    {
+        UpdateMovementInput();
+        UpdateMousePosition();
+    }
+
+    void UpdateMovementInput()
+    {
+        movementInputStatus.up = Pressed((KeyCode)Movement.up);
+        movementInputStatus.down = Pressed((KeyCode)Movement.down);
+        movementInputStatus.left = Pressed((KeyCode)Movement.left);
+        movementInputStatus.right = Pressed((KeyCode)Movement.right);
+    }
+
+    public bool WasPressed(KeyCode k)
+    {
+        return Input.GetKeyDown(k);
+    }
+
+    public bool Pressed(KeyCode k)
+    {
+        return Input.GetKey(k);
+    }
+    void UpdateMousePosition()
+    {
+        //mousePositionFlat = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f);
+        mousePositionFlat = hitInfo.point;
+        
+    }
+    
+
+}
